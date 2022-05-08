d2e4b7846b98605668901e7107883f427b2f2b74
Rumana K <rumana2k20@gmail.com>
2022-05-08 18:49:55 +0600

    logo and favicon added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo from './../../img/logo.png'
 
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand as={Link} to="/">Computer World</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/"> <img className='w-25' src={logo} alt="" srcset="" /> </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
                             <Nav.Link as={Link} to="/inventory">Inventory</Nav.Link>
                             
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import HomeInventory from '../HomeInventory/HomeInventory';
 import Banner from './../Header/Banner/Banner'
 
